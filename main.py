62334303c513d0bb0e2efe25641490ad327959c7
afsal <afsalnahas243@gmail.com>
2024-07-11 13:05:00 +0530

    HTML

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ from bank_operations.bank import credit,debit,transfer
 
 print("hello world")
 print("new program")
+
+
+class fruits:
+    def _init_(self,name,color) -> None:
+        self.name = name
+        self.color = color
+
+    def say_color(self):
+        print("my color is", self.color)
+
+    class apple(fruits):
+        def my_type(self):
+            print("i am an apple..")
+
+a = apple("apple1", "red")
+
+print(a.color)
+                     
+                    
